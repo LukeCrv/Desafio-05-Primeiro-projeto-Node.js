d15378fce5b16ba7ac8841afec551c353686db42
Luke Carvalho <lucas.docs.mail@gmail.com>
2020-05-19 04:40:35 +0000

    Desafio concluido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface Data {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +19,30 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = { income: 0, outcome: 0, total: 0 };
+
+    this.transactions.forEach(element => {
+      if (element.type === 'income') {
+        balance.income += element.value;
+        balance.total += element.value;
+      }
+      if (element.type === 'outcome') {
+        balance.outcome += element.value;
+        balance.total -= element.value;
+      }
+    });
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Data): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
